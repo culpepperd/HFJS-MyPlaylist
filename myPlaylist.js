0823b6ff2329c3f6876a084dee4c6a0a05210c8d
culpepperd <culpepperd2490@gmail.com>
2023-06-15 14:39:20 -0400

    added CSS to .html file and code to change colors in .js file

--- myPlaylist.js
+++ myPlaylist.js
@@ -2,9 +2,13 @@ function addSongs() {
     var favSong = prompt("What's your favorite song?");
     var favArtist = prompt("And your favorite artist?");
     var song1 = document.getElementById("song1");
+    song1.setAttribute("class", "redtext");
     var song2 = document.getElementById("song2");
+    song2.setAttribute("class", "bluetext");
     var song3 = document.getElementById("song3");
+    song3.setAttribute("class", "greentext");
     var song4 = document.getElementById("song4");
+    song4.setAttribute("class", "purptext");
 
     song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
     song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
