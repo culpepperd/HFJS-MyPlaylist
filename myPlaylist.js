1914346102c79f06e8a65f93501486ab9d009fa2
culpepperd <culpepperd2490@gmail.com>
2023-06-14 23:59:31 -0400

    testing

--- myPlaylist.js
+++ myPlaylist.js
@@ -1,4 +1,5 @@
 function addSongs() {
+    var favSong = prompt("What's your favorite song?");
     var song1 = document.getElementById("song1");
     var song2 = document.getElementById("song2");
     var song3 = document.getElementById("song3");
@@ -7,9 +8,6 @@ function addSongs() {
     song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
     song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
     song3.innerHTML = "I Code the Line, by Johnny JavaScript";
-    
-    var favSong = prompt("What's your favorite song?");
     song4.innerHTML = favSong;
-    //console.log(favSong);
 }
 window.onload = addSongs;
